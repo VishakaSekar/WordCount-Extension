ee2b8e1e275e8143f2936286a0a466ca2fd0734b
VishakaSekar <vishaka.sekar@appdynamics.com>
2018-05-23 17:11:33 -0700

    made code compatible with 2.0 framework

---
+++ src/main/java/com/appdynamics/extensions/wordcounter/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.appdynamics.extensions.wordcounter.utils;
+
+public class Constants {
+
+        public static final String DEFAULT_METRIC_PREFIX = "Custom Metrics|Word Count|";
+        public static final String METRIC_SEPARATOR  = "|";
+
+}
